a0c35f1d363d8c6e45c4a9f86094195d4e46ecb0
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-02-12 08:12:47 +1100

    implemented throw-env

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,25 +1,8 @@
+import throwEnv from "throw-env";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const requiredEnvVars = ["NODE_ENV", "ACCESS_KEY", "MONGO_URI"];
-
-/**
- * Fetch the value of an environment variable
- * Throw an error if the variable is undefined
- *
- * @param {string | undefined} envVar The environment variable you want to fetch
- * @returns {string} If the environment variable is defined, return its value
- */
-const fetchEnvOrThrow = (envVar: string | undefined): string => {
-	if (!envVar)
-		throw Error(
-			"A required environment variable is undefined. The required environment variables are: " +
-				requiredEnvVars
-		);
-	return envVar as string;
-};
-
 export const PORT = process.env.PORT;
-export const NODE_ENV = fetchEnvOrThrow(process.env.NODE_ENV);
-export const MONGO_URI = fetchEnvOrThrow(process.env.MONGO_URI);
-export const ACCESS_KEY = fetchEnvOrThrow(process.env.ACCESS_KEY);
+export const NODE_ENV = throwEnv("NODE_ENV");
+export const MONGO_URI = throwEnv("MONGO_URI");
+export const ACCESS_KEY = throwEnv("ACCESS_KEY");
